fae8f7148a6e5bfdea842f5925a99c9293f65c6b
Hanzo Alpha <hansen.makangiras@hotmail.com>
2025-01-29 09:39:23 +0800

    Perbaiki format kode di app.js
    Menambahkan titik koma untuk konsistensi format kode pada impor dan pemanggilan plugin. Perubahan ini tidak mengubah fungsionalitas aplikasi, hanya meningkatkan keterbacaan dan standar penulisan kode.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 import {Alpine, Livewire} from '../../vendor/livewire/livewire/dist/livewire.esm';
-import Clipboard from '@ryangjchandler/alpine-clipboard'
+import Clipboard from '@ryangjchandler/alpine-clipboard';
 
-Alpine.plugin(Clipboard)
+Alpine.plugin(Clipboard);
 
-Livewire.start()
+Livewire.start();
