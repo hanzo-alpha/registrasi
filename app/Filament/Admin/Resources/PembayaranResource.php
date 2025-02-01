b92a185813bd07a8ae2fb2bc5408916c73ab1f49
Hanzo Alpha <hansen.makangiras@hotmail.com>
2025-02-01 23:54:06 +0800

    detail pembayaran view

--- app/Filament/Admin/Resources/PembayaranResource.php
+++ app/Filament/Admin/Resources/PembayaranResource.php
@@ -14,6 +14,10 @@ use App\Filament\Admin\Resources\PembayaranResource\Pages;
 use App\Models\Pembayaran;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Group;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -103,46 +107,159 @@ class PembayaranResource extends Resource
             ]);
     }
 
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist->schema([
+            Group::make()->schema([
+                Section::make('Data Peserta')->schema([
+                    TextEntry::make('earlybird.nama_lengkap')
+                        ->label('Nama Lengkap')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.email')
+                        ->label('Email')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.no_telp')
+                        ->label('No Telp')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.jenis_kelamin')
+                        ->label('Jenis Kelamin')
+                        ->badge(),
+                    TextEntry::make('earlybird.tempat_lahir')
+                        ->label('Tempat Lahir')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.tanggal_lahir')
+                        ->label('Tanggal Lahir')
+                        ->date('d M Y')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.alamat')
+                        ->label('Alamat')
+                        ->columnSpanFull()
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.negara')
+                        ->label('Negara')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.prov.name')
+                        ->label('Provinsi')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.kab.name')
+                        ->label('Kabupaten')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.kec.name')
+                        ->label('Kecamatan')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.tipe_kartu_identitas')
+                        ->badge()
+                        ->label('Tipe Kartu Identitas')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.nomor_kartu_identitas')
+                        ->label('Nomor Kartu Identitas')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.nama_kontak_darurat')
+                        ->label('Nama Kontak Darurat')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.nomor_kontak_darurat')
+                        ->label('Nomor Kontak Darurat')
+                        ->color('secondary'),
+                    TextEntry::make('earlybird.golongan_darah')
+                        ->badge()
+                        ->label('Golongan Darah'),
+                ])->columns(2),
+            ])->columnSpan(2),
+            Group::make()->schema([
+                Section::make('Status Peserta')->schema([
+
+                    TextEntry::make('earlybird.ukuran_jersey')->badge(),
+                    TextEntry::make('earlybird.kategori.nama')->badge(),
+                    TextEntry::make('earlybird.komunitas')->badge(),
+                    TextEntry::make('earlybird.status_earlybird')->badge(),
+                ])->columns(2),
+                Section::make('Pembayaran')->schema([
+                    TextEntry::make('tipe_pembayaran')
+                        ->label('Tipe Bayar')->badge(),
+                    TextEntry::make('status_pembayaran')
+                        ->label('Status Pembayaran')->badge(),
+                    TextEntry::make('status_pendaftaran')
+                        ->label('Status Pendaftaran')->badge(),
+                    TextEntry::make('status_transaksi')
+                        ->label('Status Transaksi')->badge(),
+                ])->columns(2),
+            ])->columns(1),
+        ])->columns(3);
+    }
+
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('uuid_pembayaran')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('registrasi.id')
-                    ->numeric()
-                    ->sortable(),
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('earlybird.nama_lengkap')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: false),
                 Tables\Columns\TextColumn::make('nama_kegiatan')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('ukuran_jersey')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('kategori_lomba')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->badge()
+                    ->toggleable(isToggledHiddenByDefault: false),
+                Tables\Columns\TextColumn::make('kategori.nama')
+                    ->searchable()
+                    ->sortable()
+                    ->badge()
+                    ->toggleable(isToggledHiddenByDefault: false),
                 Tables\Columns\TextColumn::make('jumlah')
                     ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('satuan')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('harga_satuan')
                     ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('total_harga')
                     ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('tipe_pembayaran')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->badge()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('status_pembayaran')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->badge()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('status_transaksi')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->badge()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\IconColumn::make('status_daftar')
-                    ->boolean(),
+                    ->boolean()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('status_pendaftaran')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: false),
                 Tables\Columns\TextColumn::make('keterangan')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('lampiran')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -156,6 +273,7 @@ class PembayaranResource extends Resource
 
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -178,6 +296,7 @@ class PembayaranResource extends Resource
             'index' => Pages\ListPembayarans::route('/'),
             'create' => Pages\CreatePembayaran::route('/create'),
             'edit' => Pages\EditPembayaran::route('/{record}/edit'),
+            'view' => Pages\ViewPembayaran::route('/{record}/view'),
         ];
     }
 }
