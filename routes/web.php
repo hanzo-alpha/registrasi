7c22386732d57a4a23311f5db0f9175122596593
Hanzo Alpha <hansen.makangiras@hotmail.com>
2025-02-07 19:49:31 +0800

    `Tambahkan fitur eksport Earlybird`
    Membuat kelas `EarlybirdExporter` untuk mendukung proses eksport data pendaftar Earlybird. Menyertakan kolom-kolom penting seperti data pribadi, kontak darurat, dan status pendaftaran, serta notifikasi hasil eksport.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use App\Livewire\Frontend\Index;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', Index::class)->name('home');
-//Route::get('/', fn() => redirect()->route('filament.app.pages.earlybird'))->name('home');
-Route::get('/', fn() => redirect()->route('filament.app.pages.pendaftaran'))->name('home');
+Route::get('/', fn() => redirect()->route('filament.app.pages.earlybird'))->name('home');
+//Route::get('/', fn() => redirect()->route('filament.app.pages.pendaftaran'))->name('home');
 Route::webhooks('registrasi-webhook');
