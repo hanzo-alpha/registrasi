13dc1358ab75cbadca24a7ca45f6ad3e5d7c61ab
Hanzo Alpha <hansen.makangiras@hotmail.com>
2025-02-12 11:32:45 +0800

    Disable sebelum buka pendaftaran

--- app/Filament/App/Pages/Earlybird.php
+++ app/Filament/App/Pages/Earlybird.php
@@ -232,8 +232,15 @@ class Earlybird extends Page implements HasForms
                                     ->required(),
                             ])->columns(2),
                     ]),
-            ])->submitAction(new HtmlString(Blade::render(
-                <<<BLADE
+            ])
+//                ->previousAction(
+//                    fn(Forms\Components\Actions\Action $action) => $action->disabled(),
+//                )
+//                ->nextAction(
+//                    fn(Forms\Components\Actions\Action $action) => $action->disabled(),
+//                )
+                ->submitAction(new HtmlString(Blade::render(
+                    <<<BLADE
                 <x-filament::button
                     type="submit"
                     size="sm"
@@ -241,7 +248,7 @@ class Earlybird extends Page implements HasForms
                     Submit
                 </x-filament::button>
             BLADE,
-            )))
+                )))
                 ->columnSpanFull(),
         ];
     }
