774554d18a4292242ae942f7c6eea536e55293d5
Hanzo Alpha <hansen.makangiras@hotmail.com>
2025-04-22 12:29:28 +0800

    Tambahkan statistik kategori pembayaran dan pendaftaran
    Menambahkan statistik baru untuk kategori "early bird" dan "normal" pada widget PembayaranOverview dan PendaftaranOverview. Memperbaiki tampilan data statistik agar lebih informatif terkait kategori pembayaran dan pendaftaran peserta.

--- app/Filament/Admin/Widgets/PembayaranOverview.php
+++ app/Filament/Admin/Widgets/PembayaranOverview.php
@@ -15,8 +15,20 @@ class PembayaranOverview extends BaseWidget
     protected function getStats(): array
     {
         $totalBayarLunas = Pembayaran::query()
+            ->whereHas('pendaftaran', fn($query) => $query->whereHas('kategori', fn($query) => $query->where('kategori', 'early_bird')))
             ->where('status_pembayaran', StatusBayar::SUDAH_BAYAR)
             ->sum('total_harga');
+
+        $totalEarlyBird = Pembayaran::query()
+            ->whereHas('pendaftaran', fn($query) => $query->whereHas('kategori', fn($query) => $query->where('kategori', 'early_bird')))
+            ->where('status_pembayaran', StatusBayar::SUDAH_BAYAR)
+            ->sum('total_harga');
+
+        $totalNormal = Pembayaran::query()
+            ->whereHas('pendaftaran', fn($query) => $query->whereHas('kategori', fn($query) => $query->where('kategori', 'normal')))
+            ->where('status_pembayaran', StatusBayar::SUDAH_BAYAR)
+            ->sum('total_harga');
+
         $totalBayarLunasEarlybird = Pembayaran::where('status_pembayaran', StatusBayar::PENDING)
             ->sum('total_harga');
         $totalBayarLunasNormal = Pembayaran::where('status_pembayaran', StatusBayar::BELUM_BAYAR)
@@ -29,9 +41,15 @@ class PembayaranOverview extends BaseWidget
             Stat::make('Total Pembayaran Pending', Number::format($totalBayarLunasEarlybird, 0, null, 'id'))
                 ->description('Jumlah Pembayaran Pending')
                 ->color('yellow'),
-            Stat::make('Total Belum Membayar', Number::format($totalBayarLunasNormal, 0, null, 'id'))
-                ->description('Jumlah Belum Membayar')
-                ->color('secondary'),
+            //            Stat::make('Total Belum Membayar', Number::format($totalBayarLunasNormal, 0, null, 'id'))
+            //                ->description('Jumlah Belum Membayar')
+            //                ->color('secondary'),
+            Stat::make('Total Pembayaran Earlybird', Number::format($totalEarlyBird, 0, null, 'id'))
+                ->description('Kategori Earlybird')
+                ->color('success'),
+            Stat::make('Total Pembayaran Normal', Number::format($totalNormal, 0, null, 'id'))
+                ->description('Kategori Normal')
+                ->color('danger'),
         ];
     }
 }
--- app/Filament/Admin/Widgets/PendaftaranOverview.php
+++ app/Filament/Admin/Widgets/PendaftaranOverview.php
@@ -14,13 +14,10 @@ class PendaftaranOverview extends BaseWidget
 {
     protected function getStats(): array
     {
-        $countEarly = Pendaftaran::count();
-        $totalBayarLunas = Pendaftaran::query()
-            ->where('status_registrasi', StatusRegistrasi::BERHASIL)
-            ->count();
-        $totalFailed = Pendaftaran::query()
-            ->where('status_registrasi', StatusRegistrasi::BATAL)
-            ->count();
+        $countEarly = Pendaftaran::whereHas('kategori', fn($query) => $query->where('kategori', 'early_bird'))->count();
+        $countNormal = Pendaftaran::whereHas('kategori', fn($query) => $query->where('kategori', 'normal'))->count();
+        $countAll = Pendaftaran::count();
+
         $totalPending = Pendaftaran::query()
             ->whereIn('status_registrasi', [
                 StatusRegistrasi::TUNDA,
@@ -29,29 +26,29 @@ class PendaftaranOverview extends BaseWidget
             ->count();
         return [
             Stat::make(
-                'Jumlah Pendaftar Registrasi',
-                Number::format($countEarly, 0, null, 'id') . ' Peserta',
+                'Jumlah Semua Peserta',
+                Number::format($countAll, 0, null, 'id') . ' Peserta',
             )
-                ->description('Total Semua Pendaftar')
+                ->description('Jumlah Peserta Semua Kategori')
                 ->color('primary'),
             Stat::make(
-                'Jumlah Peserta Lunas',
-                Number::format($totalBayarLunas, 0, null, 'id') . ' Peserta',
+                'Jumlah Peserta Normal',
+                Number::format($countNormal, 0, null, 'id') . ' Peserta',
             )
-                ->description('Pembayaran Lunas')
+                ->description('Peserta Kategori Normal')
                 ->color('info'),
             Stat::make(
-                'Jumlah Peserta Pending',
-                Number::format($totalPending, 0, null, 'id') . ' Peserta',
+                'Jumlah Peserta Early Bird',
+                Number::format($countEarly, 0, null, 'id') . ' Peserta',
             )
-                ->description('Pembayaran Pending')
+                ->description('Peserta Kategori Early Bird')
                 ->color('warning'),
-            Stat::make(
-                'Jumlah Peserta Kedaluwarsa',
-                Number::format($totalFailed, 0, null, 'id') . ' Peserta',
-            )
-                ->description('Pembayaran Kedaluwarsa')
-                ->color('danger'),
+            //            Stat::make(
+            //                'Jumlah Peserta Kedaluwarsa',
+            //                Number::format($totalFailed, 0, null, 'id') . ' Peserta',
+            //            )
+            //                ->description('Pembayaran Kedaluwarsa')
+            //                ->color('danger'),
         ];
     }
 }
